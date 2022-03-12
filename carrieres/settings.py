99878876cc6e086d6ed5318332806c24c89f30f7
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-03-12 04:27:54 +0100

    update

--- carrieres/settings.py
+++ carrieres/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@-7d2c9jf_++$i@^ku9dn9f6zgu1s&vkl2#5%hkm_akcvt0=d!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
