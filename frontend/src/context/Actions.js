d41fafe5906257e31b4e5df076d1c5b3e69084cc
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-03-22 18:47:29 +0100

    update

--- frontend/src/context/Actions.js
+++ frontend/src/context/Actions.js
@@ -37,11 +37,11 @@ export const addFiche = async (name, group, rank, path, For, End, Hab, Char, Int
   return response.data;
 };
 
-export const editFiche = async (id, name, group, rank, path, For, End, Hab, Char, Int, Ini, Att, Par, Tir, Na, Pv) => {
+export const editFiche = async (id, name, group, rank, path, For, End, Hab, Char, Int, Ini, Att, Par, Tir, Na, Pv, For1V, End1V, Hab1V, Char1V, Int1V, Ini1V, Att1V, Par1V, Tir1V, Na1V, Pv1V, For2V, End2V, Hab2V, Char2V, Int2V, Ini2V, Att2V, Par2V, Tir2V, Na2V, Pv2V, For3V, End3V, Hab3V, Char3V, Int3V, Ini3V, Att3V, Par3V, Tir3V, Na3V, Pv3V, For4V, End4V, Hab4V, Char4V, Int4V, Ini4V, Att4V, Par4V, Tir4V, Na4V, Pv4V) => {
   const response = await manager({
     method: "PUT",
     url: `/api/fiches/${id}/`,
-    data: {name, group, rank, path, For, End, Hab, Char, Int, Ini, Att, Par, Tir, Na, Pv},
+    data: {name, group, rank, path, For, End, Hab, Char, Int, Ini, Att, Par, Tir, Na, Pv, For1V, End1V, Hab1V, Char1V, Int1V, Ini1V, Att1V, Par1V, Tir1V, Na1V, Pv1V, For2V, End2V, Hab2V, Char2V, Int2V, Ini2V, Att2V, Par2V, Tir2V, Na2V, Pv2V, For3V, End3V, Hab3V, Char3V, Int3V, Ini3V, Att3V, Par3V, Tir3V, Na3V, Pv3V, For4V, End4V, Hab4V, Char4V, Int4V, Ini4V, Att4V, Par4V, Tir4V, Na4V, Pv4V},
   });
 
   return response.data;
