c459151b999337a068a487be751c2ad3008c929b
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-04-18 02:16:22 +0200

    feat: models update

--- reputations/admin.py
+++ reputations/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(PeopleReputation)
+admin.site.register(NobilityReputation)
+admin.site.register(ClergyReputation)
+admin.site.register(MilitiaReputation)
+admin.site.register(BanishedReputation)
--- reputations/models.py
+++ reputations/models.py
@@ -6,7 +6,7 @@ from choices import GROUPS, STATUS
 
 class PeopleReputation(models.Model):
     owner = models.ForeignKey(
-        CharacterSheet, on_delete=models.SET_NULL, blank=True, null=True)
+        CharacterSheet, on_delete=models.CASCADE, blank=True, null=True, editable=False)
     globalStatus = models.CharField(
         max_length=50, choices=STATUS, default='Neutre', blank=True, null=True)
     kingStatus = models.CharField(
@@ -26,7 +26,7 @@ class PeopleReputation(models.Model):
 
 class ClergyReputation(models.Model):
     owner = models.ForeignKey(
-        CharacterSheet, on_delete=models.SET_NULL, blank=True, null=True)
+        CharacterSheet, on_delete=models.CASCADE, blank=True, null=True, editable=False)
     globalStatus = models.CharField(
         max_length=50, choices=STATUS, default='Positive', blank=True, null=True)
     kingStatus = models.CharField(
@@ -46,7 +46,7 @@ class ClergyReputation(models.Model):
 
 class MilitiaReputation(models.Model):
     owner = models.ForeignKey(
-        CharacterSheet, on_delete=models.SET_NULL, blank=True, null=True)
+        CharacterSheet, on_delete=models.CASCADE, blank=True, null=True, editable=False)
     globalStatus = models.CharField(
         max_length=50, choices=STATUS, default='Positive', blank=True, null=True)
     kingStatus = models.CharField(
@@ -66,7 +66,7 @@ class MilitiaReputation(models.Model):
 
 class NobilityReputation(models.Model):
     owner = models.ForeignKey(
-        CharacterSheet, on_delete=models.SET_NULL, blank=True, null=True)
+        CharacterSheet, on_delete=models.CASCADE, blank=True, null=True, editable=False)
     globalStatus = models.CharField(
         max_length=50, choices=STATUS, default='Neutre', blank=True, null=True)
     kingStatus = models.CharField(
@@ -86,19 +86,19 @@ class NobilityReputation(models.Model):
 
 class BanishedReputation(models.Model):
     owner = models.ForeignKey(
-        CharacterSheet, on_delete=models.SET_NULL, blank=True, null=True)
+        CharacterSheet, on_delete=models.CASCADE, blank=True, null=True, editable=False)
     globalStatus = models.CharField(
-        max_length=50, choices=STATUS, default='Negative', blank=True, null=True)
+        max_length=50, choices=STATUS, default='Négative', blank=True, null=True)
     kingStatus = models.CharField(
-        max_length=50, choices=STATUS, default='Negative', blank=True, null=True)
+        max_length=50, choices=STATUS, default='Négative', blank=True, null=True)
     nobilityStatus = models.CharField(
-        max_length=50, choices=STATUS, default='Positive', blank=True, null=True)
+        max_length=50, choices=STATUS, default='Négative', blank=True, null=True)
     peopleStatus = models.CharField(
-        max_length=50, choices=STATUS, default='Negative', blank=True, null=True)
+        max_length=50, choices=STATUS, default='Négative', blank=True, null=True)
     clergyStatus = models.CharField(
-        max_length=50, choices=STATUS, default='Negative', blank=True, null=True)
+        max_length=50, choices=STATUS, default='Négative', blank=True, null=True)
     labretStatus = models.CharField(
-        max_length=50, choices=STATUS, default='Negative', blank=True, null=True)
+        max_length=50, choices=STATUS, default='Négative', blank=True, null=True)
     banishedStatus = models.CharField(
         max_length=50, choices=STATUS, default='Positive', blank=True, null=True)
     mafiaStatus = models.CharField(
