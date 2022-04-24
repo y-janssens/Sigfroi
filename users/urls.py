3c83ce515e0e798469337565d3efa45f110adfd0
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-04-24 21:53:16 +0200

    update

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginUser, name="login"),
+    path('register', views.registerUser, name="register"),
     path('logout/', views.logoutUser, name="logout"),
 ]
 
