c984a85ffa0be694c228a90960b7c80ff81a3c86
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-05-04 00:21:33 +0200

    update

---
+++ static/js/selector.js
@@ -0,0 +1,52 @@
+axios.get("http://127.0.0.1:8000/api/competences/").then(function (response) {
+  skills = response.data;
+  document.getElementById("skills-list").innerHTML = "";
+  for (let i = 0; i < skills.length; i++) {
+    document.getElementById(
+      "skills-list"
+    ).innerHTML += `<div class="skill-search-content-item"><input type="checkbox" name="${skills[i].name}" /><label for="${skills[i].name}">${skills[i].name}</label></div>`;
+  }
+});
+
+let toggle = false;
+
+function panelToggle() {
+  if (toggle == false) {
+    toggle = true;
+    $("#skills-search-container").css("height", "235px");
+    $("#skills-search-container").css("padding", "5px 0");
+    $("#skills-search-container").css("border-top", "1px solid #938471");
+    $("#skills-search-container").css("border-left", "1px solid #938471");
+    $("#skills-search-container").css("border-right", "1px solid #938471");
+    $("#skills-search-container").css("border-bottom", "1px solid #938471");
+    $(".skill-search-content").css("height", "180px");
+    $(".arrow").css("transform", "rotate(-90deg)");
+    $(".skills-search-btn").css("display", "flex");
+  } else {
+    toggle = false;
+    $("#skills-search-container").css("height", "0px");
+    $("#skills-search-container").css("padding", "0");
+    $("#skills-search-container").css("border-top", "none");
+    $("#skills-search-container").css("border-left", "1px solid #938471");
+    $("#skills-search-container").css("border-right", "1px solid #938471");
+    $("#skills-search-container").css("border-bottom", "none");
+    $(".skill-search-content").css("height", "0px");
+    $(".arrow").css("transform", "rotate(90deg)");
+    $(".skills-search-btn").css("display", "none");
+  }
+}
+
+let result = document.getElementById("skill_search_bar_input");
+result.addEventListener("keyup", () => {
+  let query = result.value;
+  document.getElementById("skills-list").innerHTML = "";
+  let skillsList = skills.filter(function (skill) {
+    return skill.name.toLowerCase().includes(query?.toLowerCase());
+  });
+
+  skillsList.map((skill) => {
+    return (document.getElementById(
+      "skills-list"
+    ).innerHTML += `<div class="skill-search-content-item"><input type="checkbox" name="${skill.name}" /><label for="${skill.name}">${skill.name}</label></div>`);
+  });
+});
