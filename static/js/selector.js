6fdad949b36edd0d8147cc33d573cd4af35e8e93
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-05-11 23:34:46 +0200

    feat: (fix)

--- static/js/selector.js
+++ static/js/selector.js
@@ -1,7 +1,7 @@
 let toggle = false;
 let count = 0;
 
-axios.get("http://127.0.0.1:8000/api/competences/").then(function (response) {
+axios.get("https://carrieres-marbrume.herokuapp.com/api/competences/").then(function (response) {
   skills = response.data;
   document.getElementById("skills-list").innerHTML = "";
   for (let i = 0; i < skills.length; i++) {
