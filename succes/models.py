6acc0147aa4c6b48cf4ac3762171948501a36589
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2022-08-14 03:26:02 +0200

    feat: (achievements-fix)

--- succes/models.py
+++ succes/models.py
@@ -3,11 +3,23 @@ from fiches.models import *
 from choices import *
 from .text import *
 
+class Achievement(models.Model):
+    id = models.IntegerField(blank=False, null=False, primary_key=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    text = models.TextField(max_length=2000, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
+
+    class Meta:
+        ordering = ['id']
+
 class AchievementsSheet(models.Model):
+    achievements = Achievement.objects.all()
     owner = models.ForeignKey(CharacterSheet, on_delete=models.CASCADE, blank=True, editable=False)
 
-    for i in achievementsList:
-        locals()[i['title']] = models.BooleanField(default=False, blank=False, null=False)
+    for achievement in achievements:
+        locals()[achievement.title] = models.BooleanField(default=False, blank=False, null=False)
 
 
     def __str__(self):
