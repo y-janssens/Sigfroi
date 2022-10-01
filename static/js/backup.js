35f7acf76a496ae8c08bb36cb77ee8be9daf29d6
y-janssens <scorpiostyle@hotmail.fr>
2022-10-01 15:59:10 +0200

    feat: (loader)

--- static/js/backup.js
+++
@@ -1,6 +0,0 @@
-const handleBackup = (value, name) => {
-    const file = document.createElement('a');
-    file.href = window.URL.createObjectURL(new Blob([value], { type: 'application/json' }));
-    file.download = `${name}.json`;
-    file.click();
-};
