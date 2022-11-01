f23df8ea77affab29404f8bd5bb09c0403f39e51
y-janssens <scorpiostyle@hotmail.fr>
2022-11-01 20:55:56 +0100

    add success list view

--- succes/forms.py
+++ succes/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import AchievementsSheet
+from .models import AchievementsSheet, Achievement
 
 
 class AchievementsheetForm(ModelForm):
     class Meta:
         model = AchievementsSheet
         fields = '__all__'
+
+class AchievementForm(ModelForm):
+    class Meta:
+        model = Achievement
+        fields = ['title', 'text']
