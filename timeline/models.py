81b0e1520cea4e800980746b12c36be9a6707734
y-janssens <scorpiostyle@hotmail.fr>
2023-03-05 15:08:45 +0100

    feat: (fix-timeline-datas)

--- timeline/models.py
+++ timeline/models.py
@@ -13,7 +13,6 @@ class TimelineLink(models.Model):
 
 
 class TimelineEvent(models.Model):
-    name = models.IntegerField(blank=True, null=True)
     title = models.CharField(max_length=250, blank=True, null=True)
     year = models.IntegerField(blank=True, null=True)
     date = models.CharField(max_length=50, blank=True, null=True)
@@ -21,9 +20,10 @@ class TimelineEvent(models.Model):
     details = models.TextField(max_length=30000, blank=True, null=True)
     consequences = models.TextField(max_length=30000, blank=True, null=True)
     links = models.ManyToManyField(TimelineLink, blank=True, symmetrical=False)
+    origin = models.BooleanField(default=False, editable=False)
 
     def __str__(self):
         return f"{self.date} {self.year} - {self.title}"
 
     class Meta:
-        ordering = ['year', 'name']
+        ordering = ['year', 'id']
